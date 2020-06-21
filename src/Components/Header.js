37bd87706cb92993d2ea68c791191cda279d2de7
Aqib_Iqbal <aqibiqbal48@gmail.com>
2020-06-21 21:05:44 +0500

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
     return (
         <div>
-            <h2>Expense Tracker1</h2>
+            <h2>Expense Tracker</h2>
         </div>
     )
 }
