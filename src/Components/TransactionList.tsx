1118cc78ac3f88d03c2b4e6a0d2ec37b32fb3e4a
Aqib_Iqbal <aqibiqbal48@gmail.com>
2020-08-30 18:35:04 +0500

    Create TransactionList.tsx

---
+++ src/Components/TransactionList.tsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import { Transaction } from './Transaction'
+import { GlobalContext } from '../Context/GlobalState';
+import {transactionType} from '../Types/types'; 
+
+export const TransactionList = () => {
+    const { transactions } = useContext(GlobalContext);
+
+    return (
+        <>
+            <h3>History</h3>
+
+            <ul className="list">
+                {
+                     transactions.map((transaction: transactionType) => (transaction.text === "" && transaction.amount === 0  ? "" : <Transaction key={transaction.id} transaction = {transaction}  />))
+                    //transactions.map(transaction => (<Transaction key={transaction.id} transaction = {transaction}  />))
+                }
+
+            </ul>
+        </>
+    )
+}
