e3dcb7f7baf42d35510bc560eb9a7f6d35c4caa6
Aqib_Iqbal <aqibiqbal48@gmail.com>
2021-01-22 19:26:40 +0500

    Update TransactionList.tsx

--- src/Components/TransactionList.tsx
+++ src/Components/TransactionList.tsx
@@ -8,7 +8,7 @@ export const TransactionList = () => {
 
     return (
         <>
-            <h3>Historys</h3>
+            <h3>Historysss</h3>
 
             <ul className="list">
                 {
